812e43f84e95976c9b25be9787da9deb86384ca0
AbhayKushwah07 <abhaykushwah07@gmail.com>
2025-08-19 21:51:22 +0530

    creating libraries

---
+++ libs/user/src/lib/user.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-user',
+  imports: [],
+  template: `
+    <p>
+      user works!
+    </p>
+  `,
+  styles: ``
+})
+export class User {
+
+}
